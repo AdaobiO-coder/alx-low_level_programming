21e2f446cf2447c5c71675cbc86428b2a2f56755
AdaobiO-coder <okekeadaobi9@gmail.com>
2022-09-10 12:48:29 -0700

    8 script

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
+#include <unistd.h>
+
+/**
+* main-Entry point
+* Return: Always 1 (success)
+*/
+
 int main(void)
 {
-	int a = 5;
-	int b = 4;
-	for (a < b;;)
-	{
-		    printf("%d\n", a++);
-	}
-11:46
+		write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+
+		return (1);
 }
