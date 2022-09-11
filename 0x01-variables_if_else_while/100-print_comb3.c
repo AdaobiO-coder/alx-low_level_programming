1a97abb7e9cb2b983a3c47681f44a6410ec9bb15
AdaobiO-coder <okekeadaobi9@gmail.com>
2022-09-11 05:42:10 -0700

    print 100

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -2,21 +2,25 @@
 /**
 *main - Prints numbers between 00 to 89.
 *
-*Return:0
+*Return: Always 0 (Success)
 */
 int main(void)
 {
 int i, e;
+
 i = 48;
 e = 48;
+
 while (e < 58)
 {
 i = 48;
 while (i < 58)
 {
+if (e != i && e < i)
+{
 putchar(e);
 putchar(i);
-if (i == 57 && e ==56)
+if (i == 57 && e == 56)
 {
 break;
 }
@@ -24,7 +28,7 @@ putchar(',');
 putchar(' ');
 }
 i++;
-} 
+}
 e++;
 }
 putchar('\n');
